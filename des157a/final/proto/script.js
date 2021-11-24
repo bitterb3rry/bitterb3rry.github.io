0a16d00bea166bf00eaef691838272ce57cb4165
Julia Ma <julma@ucdavis.edu>
2021-11-24 13:30:24 -0800

    testing 1

--- des157a/final/proto/script.js
+++ des157a/final/proto/script.js
@@ -1,15 +1,24 @@
-/* (function() {
+(function() {
     'use strict';
     console.log("reading js");
 
-    const page = document.scrollingElement || document.documentElement;
+    alert("welcome to magic hour 2.0 user testing.");
+    alert("please turn on inspector and set it to the 1366x763 mode for accuracy");
+    alert("the site is only complete to the extent of having the basic skeleton and dummy text");
+    alert("enjoy your stay!");
+
+    /* const page = document.scrollingElement || document.documentElement;
     page.addEventListener('wheel', function(event) {
         if(!event.deltaY) {
             event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
             event.preventDefault();
         }
-    });
+    }); */
+
+/*     const progressBar = document.getElementById("progress-bar");
+    progressBar.addEventListener("scroll", function() {
+        progressBar.style.width = 
+    }); */
 
 }());
 
- */
