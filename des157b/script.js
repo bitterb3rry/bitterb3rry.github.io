d4ee295a22c1c67f1d959bb8f241622c81d68adc
Julia Ma <julma@ucdavis.edu>
2022-01-09 22:33:35 -0800

    portal visuals completed

--- des157b/script.js
+++ des157b/script.js
@@ -2,24 +2,32 @@
     'use strict';
 
     const button = document.querySelector('button');
+    const header = document.querySelector('header');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
+    const lightBanner = document.querySelector('#light');
+    const darkBanner = document.querySelector('#dark');
     const sections = document.querySelectorAll('section')
     let mode = 'dark';
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
+            header.className = 'switch';
             body.className = 'switch';
-            banner.className = 'switch';
+            darkBanner.className = "show";
+            lightBanner.className = 'hidden';
             button.className = 'switch';
+            button.textContent = "more soup"
             for (const section of sections) {
                 section.className = 'switch';
             }
             mode = 'light';
         } else {
+            header.removeAttribute('class');
             body.removeAttribute('class');
-            banner.removeAttribute('class');
+            darkBanner.className = "hidden";
+            lightBanner.className = 'show';
             button.removeAttribute('class');
+            button.textContent = "have soup"
             for (const section of sections) {
                 section.removeAttribute('class');
             }
