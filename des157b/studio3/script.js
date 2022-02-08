4e9b02ca4f9085f167ea60e2672b645ba57c67d9
Julia Ma <julma@ucdavis.edu>
2022-02-08 08:57:36 -0800

    add

---
+++ des157b/studio3/script.js
@@ -0,0 +1,21 @@
+(function() {
+    AOS.init();
+
+
+    new kursor({
+        type: 1
+    })
+
+
+    new fullpage('#fullpage', {
+        //options here
+        /* autoScrolling:true,
+        scrollHorizontally: true, */
+    });
+    
+    //methods
+    /* fullpage_api.setAllowScrolling(true); */
+})();
+
+
+
