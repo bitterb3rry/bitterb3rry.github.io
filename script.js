1db5c9d0d9e3c367bbb365c6e94340fceb812b12
Julia Ma <julma@ucdavis.edu>
2022-02-16 22:14:57 -0800

    front page complete

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-/* (function() {
+(function() {
     'use strict';
     console.log('reading js');
 
-    var magicHour = document.getElementById('magic-hour');
+    var blockbusterSec = document.querySelector('#blockbuster-video section');
+    var blockbusterArt = document.querySelector('#blockbuster-video article');
 
-    magicHour.addEventListener('mouseover', function(event) {
+    var magicHourSec = document.querySelector('#magic-hour section');
+    var magicHourArt = document.querySelector('#magic-hour article');
+
+    magicHourSec.addEventListener('mouseover', (event) => {
         event.preventDefault();
         console.log('hovering');
+        magicHourArt.style.animation = "articleUp 0.5s ease 0s 1 normal forwards";
+    });
 
-    })
-}()); */
+    blockbusterSec.addEventListener('mouseout', (event) => {
+        event.preventDefault();
+        console.log('hovering');
+        blockbusterArt.style.animation = "articleDown 3s ease 0s 1 normal forwards";
+    });
+
+    blockbusterSec.addEventListener('mouseover', function(event) {
+        event.preventDefault();
+        console.log('hovering');
+        blockbusterArt.style.animation = "articleUp 0.5s ease 0s 1 normal forwards";
+    });
+
+    magicHourSec.addEventListener('mouseout', function(event) {
+        event.preventDefault();
+        console.log('hovering');
+        magicHourArt.style.animation = "articleDown 3s ease 0s 1 normal forwards";
+    });
+
+}());
