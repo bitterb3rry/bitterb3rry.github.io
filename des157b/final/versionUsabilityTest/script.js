0b4f1697e07963cb8c72c894abfb69bf4159d77b
Julia Ma <julma@ucdavis.edu>
2022-03-02 15:52:10 -0800

    usability test prompt

--- des157b/final/versionUsabilityTest/script.js
+++ des157b/final/versionUsabilityTest/script.js
@@ -2,22 +2,87 @@
     Parse.initialize('cVrkrv0qAq90F6K087Atwn2lV4KDBJcNwRgPRmMA', 'oJpekGOaM9kFp9Su3vrAAlH2ZuZVE2ndSACR4ZVz');
     Parse.serverURL = 'https://parseapi.back4app.com/';
 
+    const body = document.querySelector("body");
+
+    const instruct = document.getElementById("user-instruction");
+    const closeInstruct = document.getElementById("close-instruction");
+
+    const charactersPg = document.getElementById("characters");
+    const backgroundPg = document.getElementById("background");
+    const purposePg = document.getElementById("purpose");
+
+    const toBg = document.getElementById("btn-to-bg");
+    const toPurpose = document.getElementById("btn-to-purpose");
+    const toMain = document.getElementById("btn-to-main");
+
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+
     const title = document.getElementById("title-box");
     const notesDisplay = document.querySelector("main ul");
     const addCardBtn = document.getElementById("add-card-btn");
     const addCardForm = document.getElementById("add-card-form");
-    const background = document.getElementById("background");
-    /* const backgroundBtn = document.getElementById("background-btn"); */
+    const backgroundOverlay = document.getElementById("background-overlay");
 
-/*     const cancel = document.getElementsByClassName("cancel");
+    const cancelForm = document.getElementById("cancel-form");
+    const cancelBg = document.getElementById("cancel-bg");
 
-    cancel.addEventListener("click", function(event) {
+    /* user test overlay */
+
+    closeInstruct.addEventListener("click", function(event) {
         event.preventDefault();
 
-        addCardForm.style.display = "none";
-        background.style.display = "none";
-    }) */
+        instruct.className = "hidden-display";
+        charactersPg.className = "show-display";
+    })
 
+    /* opening js */
+/*     const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
+    const characters = {
+        start: [0, 5, 8],
+        stop: [4, 7, 10],
+        line: [line1, line2, line3]
+    } */
+
+    /* opening nav buttons */
+    toBg.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        /* toBg.classList.remove = "show-display"; */
+        charactersPg.className = "hidden-display";
+        
+        /* toPurpose.classList.remove = "hidden-display"; */
+        backgroundPg.className = "show-display";
+    });
+
+    toPurpose.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        /* toBg.classList.remove = "show-display"; */
+        backgroundPg.className = "hidden-display";
+        
+        /* toPurpose.classList.remove = "hidden-display"; */
+        purposePg.className = "show-display";
+    });
+
+    toMain.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        /* toBg.classList.remove = "show-display"; */
+        purposePg.className = "hidden-display";
+        
+        /* toPurpose.classList.remove = "hidden-display"; */
+        header.className = "show-display";
+        main.className = "show-display";
+
+        body.style.backgroundColor = "#FCF9F2";
+        body.style.display = "flex";
+    });
+    /* ---------------------------------------------------- */
+
+    /* interactive nav elements */
     addCardBtn.addEventListener("mouseover", function(event) {
         addCardBtn.src = "./media/add-card-btn-action.svg";
     });
@@ -32,17 +97,12 @@
         title.src = "./media/title-box.svg";
     });
 
+    /* open input form */
     addCardBtn.addEventListener("click", function(event) {
         event.preventDefault();
-        console.log("btn clicked")
+        /* console.log("btn clicked") */
         addCardForm.style.display = "block";
     });
-
-    /* addCardForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        console.log("form submit")
-        addCardForm.style.display = "none";
-    }); */
 
     // listen for input form submit
     addCardForm.addEventListener('submit', function(event){
@@ -54,24 +114,39 @@
 
         var name = document.getElementById("name").value;
         var note = document.getElementById("note").value;
-        var flavor = document.getElementById("flavor").value;
+        /* var flavor = document.getElementById("flavor").value; */
 
         // check if there is input
-        uploadInputForm(name, note, flavor);
+        /* uploadInputForm(name, note, flavor); */
+        uploadInputForm(name, note);
 
         addCardForm.reset();
+        document.querySelectorAll("#flavor-container input").checked= "false";
 
         /* add loading screen before this reload to inform the user */
         window.location.reload();
     });
 
+    /* close overlays */
+    cancelForm.addEventListener("click", function(event) {
+        event.preventDefault();
+        addCardForm.reset();
+        document.querySelectorAll("#flavor-container input").checked= "false";
+        addCardForm.style.display = "none";
+    })
 
+    cancelBg.addEventListener("click", function(event) {
+        event.preventDefault();
+        backgroundOverlay.style.display = "none";
+    })
+
+    /* database functions */
     async function uploadInputForm(name, note, flavor) {
         const card = new Parse.Object('Notes');
 
         card.set("Name", name);
         card.set("Notes", note);
-        card.set("flavor", flavor);
+        /* card.set("flavor", flavor); */
 
         card.save().then((card) => {
             // Success
@@ -118,11 +193,4 @@
     };
 
     displayCards();
-
-/*     backgroundBtn.addEventListener("click", function(event) {
-        event.preventDefault();
-
-        background.style.display = "block";
-        addCardForm.style.display = "none";
-    }) */
 })();
